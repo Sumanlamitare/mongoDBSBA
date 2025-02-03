418d2fe645b5a4e2d3b72420e180a3a879e16030
Suman Adhikari <sumanadhikari@Sumans-MacBook-Air.local>
2025-02-03 16:14:38 -0500

    add reflection for the reflection questions

---
+++ Reflection.js
@@ -0,0 +1,14 @@
+//Reflection
+
+// What could you have done differently during the planning stages of your project to make the execution easier?
+// - i couldve planned better for the sample data because i wouldve gained a better understanding of mongo db cluster creation
+// Were there any requirements that were difficult to implement? What do you think would make them easier to implement in future projects?
+
+// -- Sample data when the program starts was a little harder than other concept
+
+//Were there any requirements that were difficult to implement? What do you think would make them easier to implement in future projects?
+// -- More functionality within the routes, better validation, accessing other collection from a different collection
+
+// Were there any requirements that were difficult to implement? What do you think would make them easier to implement in future projects?
+
+// -- Challange yourself
